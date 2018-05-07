d3b3fcc13f5bd765b8d983454081b0113bf9d960
Sindhu-Sekar <ssekar@student.unimelb.edu.au>
2018-05-07 18:00:29 +1000

    CSS changes

--- cluster_project/public/javascripts/controller.js
+++ cluster_project/public/javascripts/controller.js
@@ -1,7 +1,7 @@
 function openTab(evt, name) {
     // Declare all variables
     var i, tabcontent, tablinks;
-    var mymap = L.map('mapid').setView([-37.815,144.946], 8);
+    var mymap = L.map('mapid').setView([-37.814, 144.96332], 10);
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18,
