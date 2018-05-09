d6c033941af0ab401a124ff24665b1c83a6c45f5
JokerswildX <andres.medina.br@gmail.com>
2018-05-09 15:10:24 +1000

    minor fix in orchestrator code

--- boto/orchestrator.sh
+++ boto/orchestrator.sh
@@ -1,14 +1,48 @@
+#!/bin/bash
 
 # Boto script is required as a parameter in order to run the program
 
-if (( $# != 1 )); then
-    echo "ERROR: Wrong number of parameters. Boto script missing"
+if (( $# != 8 )); then
+    echo "ERROR: Wrong number of parameters."
+    echo "The program should use the following 3 parameters:"
+    echo "    -b 'boto_executable' "
+    echo "    -h 'ansible_host_file'"
+    echo "    -k 'ssh_key_file'"
+    echo "    -a 'ansible_file'"
     exit -1
 fi
 
+while test $# -gt 0; do
+        case "$1" in
+                -h)
+                        host=$2
+                        shift
+                        shift
+                        ;;
+                -b)
+                        boto=$2
+                        shift
+                        shift
+                        ;;
+                -k)
+                        key=$2
+                        shift
+                        shift
+                        ;;
+                -a)
+                        ansible=$2
+                        shift
+                        shift
+                        ;;
+
+                *)
+                        break
+                        ;;
+        esac
+done
 
 # The output is the program (IP address) is saved into 'result' variable
-result=`python $1`
+result=`python $boto`
 
 if [ "$?" -ne 0 ]; then
     echo "ERROR: Unexpected error while creating new instance";
@@ -20,9 +54,7 @@ echo "Instance created successfully. New instance IP: $result"
 
 
 # The IP address is added to the ansible hosts file
-echo $result >> '/home/ubuntu/ansible/hosts'
-echo "Host $result" >> '/home/ubuntu/.ssh/config'
-echo "  IdentityFile ~/.ssh/id" >> '/home/ubuntu/.ssh/config'
+echo $result >> $host
 
 
 # We wait for the instance to have the SSH service ready
@@ -31,4 +63,4 @@ echo "Waiting for SSH service for starting deployment"
 sleep 100
 
 # We run ansible playbook
-ansible-playbook '/home/ubuntu/ansible/install.yml' -i '/home/ubuntu/ansible/hosts' -u ubuntu
+ansible-playbook $ansible -i $host -u ubuntu --key-file=$key
